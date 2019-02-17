32daff0c8cd7ab3b73b1954bff39c9bdd3f1f904
HyCraftHD <hycrafthd@live.de>
2019-02-17 21:59:22 +0100

    Added item group

---
+++ src/main/java/info/u_team/useful_backpacks/init/UsefulBackPacksItemGroups.java
@@ -0,0 +1,14 @@
+package info.u_team.useful_backpacks.init;
+
+import info.u_team.u_team_core.itemgroup.UItemGroup;
+import info.u_team.useful_backpacks.UsefulBackpacksMod;
+
+public class UsefulBackPacksItemGroups {
+	
+	public static final UItemGroup group = new UItemGroup(UsefulBackpacksMod.modid, "group");
+	
+	public static void setup() {
+//		group.setIcon();
+	}
+	
+}
