a08ae997f77bbf512d0f55091e2b2f3acc292ff2
HyCraftHD <hycrafthd@live.de>
2019-02-22 01:46:50 +0100

    Fixed bug in uteamcore so now its working (120ddb8bf084164c6c43530d78b2f66ac4d5d448)

--- src/main/java/info/u_team/useful_backpacks/init/UsefulBackPacksColors.java
+++ src/main/java/info/u_team/useful_backpacks/init/UsefulBackPacksColors.java
@@ -2,6 +2,7 @@ package info.u_team.useful_backpacks.init;
 
 import info.u_team.u_team_core.registry.ColorsRegistry;
 import info.u_team.useful_backpacks.item.ItemBackPack;
+import net.minecraft.item.Item;
 import net.minecraftforge.api.distmarker.*;
 
 @OnlyIn(Dist.CLIENT)
@@ -9,8 +10,9 @@ public class UsefulBackPacksColors {
 	
 	public static void construct() {
 		ColorsRegistry.register((itemstack, index) -> {
-			if (itemstack.getItem() instanceof ItemBackPack) {
-				return ((ItemBackPack) itemstack.getItem()).getColor(itemstack);
+			Item item = itemstack.getItem();
+			if (item instanceof ItemBackPack) {
+				return ((ItemBackPack) item).getColor(itemstack);
 			}
 			return 0;
 		}, UsefulBackPacksItems.small, UsefulBackPacksItems.medium, UsefulBackPacksItems.large);
