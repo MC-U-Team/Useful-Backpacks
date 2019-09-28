0471f7b7318d708c952fbde979ccfbd5da2a929e
HyCraftHD <hycrafthd@live.de>
2019-09-28 12:28:44 +0200

    Remove some unnecessary code

--- src/main/java/info/u_team/useful_backpacks/gui/BackpackScreen.java
+++ src/main/java/info/u_team/useful_backpacks/gui/BackpackScreen.java
@@ -1,7 +1,5 @@
 package info.u_team.useful_backpacks.gui;
 
-import com.mojang.blaze3d.platform.GlStateManager;
-
 import info.u_team.u_team_core.gui.UContainerScreen;
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
 import info.u_team.useful_backpacks.container.BackpackContainer;
@@ -16,6 +14,7 @@ public class BackpackScreen extends UContainerScreen<BackpackContainer> {
 	
 	public BackpackScreen(BackpackContainer container, PlayerInventory playerInventory, ITextComponent title) {
 		super(container, playerInventory, title, new ResourceLocation(UsefulBackpacksMod.MODID, "textures/gui/backpack/" + container.getBackpack().getName() + ".png"));
+		backgroundWidth = backgroundHeight = 512;
 		final Backpack backpack = container.getBackpack();
 		xSize = backpack.getTextureSizeX();
 		ySize = backpack.getTextureSizeY();
@@ -31,14 +30,5 @@ public class BackpackScreen extends UContainerScreen<BackpackContainer> {
 	@Override
 	public void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
 		font.drawString(title.getFormattedText(), 8, 9, 4210752);
-	}
-	
-	@Override
-	public void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-		GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-		minecraft.getTextureManager().bindTexture(background);
-		int xStart = (width - xSize) / 2;
-		int yStart = (height - ySize) / 2;
-		blit(xStart, yStart, 0, 0, xSize, ySize, 512, 512);
 	}
 }
