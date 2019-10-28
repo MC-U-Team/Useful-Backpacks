b19f37c1ee044c9fc6ca7342c1cc6f238f0165ea
HyCraftHD <hycrafthd@live.de>
2019-10-28 13:16:46 +0100

    Update uteamcore version

--- src/main/java/info/u_team/useful_backpacks/UsefulBackPacksConstants.java
+++ src/main/java/info/u_team/useful_backpacks/UsefulBackPacksConstants.java
@@ -8,7 +8,7 @@ public class UsefulBackPacksConstants {
 	public static final String NAME = "Useful Backpacks";
 	public static final String VERSION = "${version}";
 	public static final String MCVERSION = "${mcversion}";
-	public static final String DEPENDENCIES = "required:forge@[14.23.5.2768,);required-after:uteamcore@[2.2.4.107,);";
+	public static final String DEPENDENCIES = "required:forge@[14.23.5.2768,);required-after:uteamcore@[2.2.4.133,);";
 	public static final String UPDATEURL = "https://api.u-team.info/update/usefulbackpacks.json";
 	
 	public static final String COMMONPROXY = "info.u_team.useful_backpacks.proxy.CommonProxy";
