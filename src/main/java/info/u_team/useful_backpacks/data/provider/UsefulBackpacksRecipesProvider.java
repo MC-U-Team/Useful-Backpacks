335e8d47338c1ff5e0d08e78794085c7df0597d0
HyCraftHD <hycrafthd@live.de>
2020-07-07 13:25:53 +0200

    Kinda fix the useful recipes provider for now

--- src/main/java/info/u_team/useful_backpacks/data/provider/UsefulBackpacksRecipesProvider.java
+++ src/main/java/info/u_team/useful_backpacks/data/provider/UsefulBackpacksRecipesProvider.java
@@ -2,13 +2,21 @@ package info.u_team.useful_backpacks.data.provider;
 
 import static info.u_team.useful_backpacks.init.UsefulBackpacksItems.*;
 
+import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import info.u_team.u_team_core.data.*;
+import info.u_team.u_team_core.util.TagUtil;
 import info.u_team.useful_backpacks.data.builder.BackpackCraftingRecipeBuilder;
+import net.minecraft.advancements.criterion.*;
+import net.minecraft.advancements.criterion.MinMaxBounds.IntBound;
 import net.minecraft.data.*;
-import net.minecraft.item.Items;
-import net.minecraft.tags.ItemTags;
+import net.minecraft.item.*;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.Ingredient.TagList;
+import net.minecraft.tags.*;
+import net.minecraft.util.IItemProvider;
 
 public class UsefulBackpacksRecipesProvider extends CommonRecipesProvider {
 	
@@ -60,4 +68,29 @@ public class UsefulBackpacksRecipesProvider extends CommonRecipesProvider {
 				.addCriterion("has_enderchest", hasItem(Items.ENDER_CHEST)) //
 				.build(consumer);
 	}
+	
+	// TEST ---- REMOVEL LATER
+	protected InventoryChangeTrigger.Instance hasItem(ITag<Item> tag) {
+		return hasItem(ItemPredicate.Builder.create().tag(tag).build());
+	}
+	
+	protected InventoryChangeTrigger.Instance hasItem(IItemProvider item) {
+		return hasItem(ItemPredicate.Builder.create().item(item).build());
+	}
+	
+	protected InventoryChangeTrigger.Instance hasItem(ItemPredicate... predicates) {
+		return new InventoryChangeTrigger.Instance(EntityPredicate.AndPredicate.field_234582_a_, IntBound.UNBOUNDED, IntBound.UNBOUNDED, IntBound.UNBOUNDED, predicates);
+	}
+	
+	public static Ingredient getIngredientOfTag(ITag<Item> tag) {
+		return Ingredient.fromItemListStream(Stream.of(new TagList(tag) {
+			
+			@Override
+			public Collection<ItemStack> getStacks() {
+				return Arrays.asList(new ItemStack(Items.ACACIA_BOAT)); // Return default value, so the ingredient will serialize our tag.
+			}
+		}));
+	}
+	
+	// TEST
 }
