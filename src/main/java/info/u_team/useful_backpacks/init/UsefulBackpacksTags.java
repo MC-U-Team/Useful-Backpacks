58273b9a132900ebc2403437695c47730f5bfe9b
HyCraftHD <hycrafthd@live.de>
2020-07-07 13:14:38 +0200

    Fix i named tag

--- src/main/java/info/u_team/useful_backpacks/init/UsefulBackpacksTags.java
+++ src/main/java/info/u_team/useful_backpacks/init/UsefulBackpacksTags.java
@@ -3,13 +3,13 @@ package info.u_team.useful_backpacks.init;
 import info.u_team.u_team_core.util.TagUtil;
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
 import net.minecraft.item.Item;
-import net.minecraft.tags.Tag;
+import net.minecraft.tags.ITag.INamedTag;
 
 public class UsefulBackpacksTags {
 	
 	public static class Items {
 		
-		public static final Tag<Item> BACKPACK = TagUtil.createItemTag(UsefulBackpacksMod.MODID, "backpack");
+		public static final INamedTag<Item> BACKPACK = TagUtil.createItemTag(UsefulBackpacksMod.MODID, "backpack");
 	}
 	
 }
