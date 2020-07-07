2233618845a82fa19a77d649c2710a6791abe82b
HyCraftHD <hycrafthd@live.de>
2020-07-07 13:20:32 +0200

    Add dummy useful backpacks block tags provider

---
+++ src/main/java/info/u_team/useful_backpacks/data/provider/UsefulBackpacksBlockTagsProvider.java
@@ -0,0 +1,15 @@
+package info.u_team.useful_backpacks.data.provider;
+
+import info.u_team.u_team_core.data.*;
+
+public class UsefulBackpacksBlockTagsProvider extends CommonBlockTagsProvider {
+	
+	public UsefulBackpacksBlockTagsProvider(GenerationData data) {
+		super(data);
+	}
+	
+	@Override
+	protected void registerTags() {
+	}
+	
+}
