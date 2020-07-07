242bf4216aaa1e965b94ecca00ed1f0e98add33f
HyCraftHD <hycrafthd@live.de>
2020-07-07 13:28:24 +0200

    Cleanup backpack screen. Compiles now

--- src/main/java/info/u_team/useful_backpacks/screen/BackpackScreen.java
+++ src/main/java/info/u_team/useful_backpacks/screen/BackpackScreen.java
@@ -1,34 +1,20 @@
 package info.u_team.useful_backpacks.screen;
 
-import info.u_team.u_team_core.gui.UContainerScreen;
+import info.u_team.u_team_core.screen.UBasicContainerScreen;
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
 import info.u_team.useful_backpacks.container.BackpackContainer;
-import info.u_team.useful_backpacks.type.Backpack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.api.distmarker.*;
 
 @OnlyIn(Dist.CLIENT)
-public class BackpackScreen extends UContainerScreen<BackpackContainer> {
+public class BackpackScreen extends UBasicContainerScreen<BackpackContainer> {
 	
 	public BackpackScreen(BackpackContainer container, PlayerInventory playerInventory, ITextComponent title) {
-		super(container, playerInventory, title, new ResourceLocation(UsefulBackpacksMod.MODID, "textures/gui/backpack/" + container.getBackpack().getName() + ".png"));
+		super(container, playerInventory, title, new ResourceLocation(UsefulBackpacksMod.MODID, "textures/gui/backpack/" + container.getBackpack().getName() + ".png"), container.getBackpack().getTextureSizeX(), container.getBackpack().getTextureSizeY());
 		backgroundWidth = backgroundHeight = 512;
-		final Backpack backpack = container.getBackpack();
-		xSize = backpack.getTextureSizeX();
-		ySize = backpack.getTextureSizeY();
-	}
-	
-	@Override
-	public void render(int mouseX, int mouseY, float partialTicks) {
-		renderBackground();
-		super.render(mouseX, mouseY, partialTicks);
-		renderHoveredToolTip(mouseX, mouseY);
-	}
-	
-	@Override
-	public void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-		font.drawString(title.getFormattedText(), 8, 9, 4210752);
+		drawInventoryText = false;
+		setTextLocation(8, 9, 0, 0);
 	}
 }
