d4934f41ab17405bc2df2a4bdecdc91ea885c62b
HyCraftHD <hycrafthd@live.de>
2020-07-16 17:50:28 +0200

    Add JeiPlugin annotation

--- src/integration/java/info/u_team/useful_backpacks/integration/jei/UsefulBackpacksJeiPlugin.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/jei/UsefulBackpacksJeiPlugin.java
@@ -1,9 +1,10 @@
 package info.u_team.useful_backpacks.integration.jei;
 
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
-import mezz.jei.api.IModPlugin;
+import mezz.jei.api.*;
 import net.minecraft.util.ResourceLocation;
 
+@JeiPlugin
 public class UsefulBackpacksJeiPlugin implements IModPlugin {
 	
 	private final ResourceLocation id = new ResourceLocation(UsefulBackpacksMod.MODID, "jei");
