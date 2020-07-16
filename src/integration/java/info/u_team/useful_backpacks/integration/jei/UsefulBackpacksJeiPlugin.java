583270c868ec3f3e4e4c4b2fa57cbb779d239f2e
HyCraftHD <hycrafthd@live.de>
2020-07-16 19:59:08 +0200

    Register the backpack item color as sub type to jei

--- src/integration/java/info/u_team/useful_backpacks/integration/jei/UsefulBackpacksJeiPlugin.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/jei/UsefulBackpacksJeiPlugin.java
@@ -1,7 +1,11 @@
 package info.u_team.useful_backpacks.integration.jei;
 
+import info.u_team.u_team_core.api.dye.IDyeableItem;
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
+import info.u_team.useful_backpacks.init.UsefulBackpacksItems;
 import mezz.jei.api.*;
+import mezz.jei.api.ingredients.subtypes.ISubtypeInterpreter;
+import mezz.jei.api.registration.ISubtypeRegistration;
 import net.minecraft.util.ResourceLocation;
 
 @JeiPlugin
@@ -12,6 +16,21 @@ public class UsefulBackpacksJeiPlugin implements IModPlugin {
 	@Override
 	public ResourceLocation getPluginUid() {
 		return id;
+	}
+	
+	@Override
+	public void registerItemSubtypes(ISubtypeRegistration registration) {
+		final ISubtypeInterpreter interpreter = stack -> {
+			if (stack.getItem() instanceof IDyeableItem) {
+				final IDyeableItem item = (IDyeableItem) stack.getItem();
+				return Integer.toString(item.getColor(stack));
+			}
+			return ISubtypeInterpreter.NONE;
+		};
+		
+		registration.registerSubtypeInterpreter(UsefulBackpacksItems.SMALL_BACKPACK.get(), interpreter);
+		registration.registerSubtypeInterpreter(UsefulBackpacksItems.MEDIUM_BACKPACK.get(), interpreter);
+		registration.registerSubtypeInterpreter(UsefulBackpacksItems.LARGE_BACKPACK.get(), interpreter);
 	}
 	
 }
