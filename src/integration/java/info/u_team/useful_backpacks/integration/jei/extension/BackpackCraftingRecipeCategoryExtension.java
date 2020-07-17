933ca47941e564057d77b57109e7fb3c3f36dc77
HyCraftHD <hycrafthd@live.de>
2020-07-17 11:42:03 +0200

    Implement the proper rotation for backpack recipes

--- src/integration/java/info/u_team/useful_backpacks/integration/jei/extension/BackpackCraftingRecipeCategoryExtension.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/jei/extension/BackpackCraftingRecipeCategoryExtension.java
@@ -7,46 +7,26 @@ import info.u_team.u_team_core.api.dye.IDyeableItem;
 import info.u_team.u_team_core.util.ColorUtil;
 import info.u_team.useful_backpacks.recipe.BackpackCraftingRecipe;
 import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.ingredient.*;
 import mezz.jei.api.ingredients.IIngredients;
-import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategoryExtension;
+import mezz.jei.api.recipe.IFocus;
+import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICustomCraftingCategoryExtension;
 import net.minecraft.block.*;
 import net.minecraft.item.*;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.util.Size2i;
 
-public class BackpackCraftingRecipeCategoryExtension implements ICraftingCategoryExtension {
+public class BackpackCraftingRecipeCategoryExtension implements ICustomCraftingCategoryExtension {
 	
 	private final BackpackCraftingRecipe recipe;
 	
+	private final ICraftingGridHelper craftingGridHelper;
+	
 	public BackpackCraftingRecipeCategoryExtension(BackpackCraftingRecipe recipe) {
 		this.recipe = recipe;
-	}
-	
-	@Override
-	public void setIngredients(IIngredients ingredients) {
-		System.out.println("YSIJSHZFSKDLFHJSKLDJFHSDLKFHSDKLJFHS");
-		ingredients.setInputIngredients(recipe.getIngredients());
-		final Optional<Ingredient> oneWoolIngredient = recipe.getIngredients().stream().filter(ingredient -> ingredient.test(new ItemStack(Blocks.BLACK_WOOL))).findAny();
-		if (oneWoolIngredient.isPresent()) {
-			final ItemStack[] matchingStacks = oneWoolIngredient.get().getMatchingStacks();
-			final List<ItemStack> outputs = Stream.of(matchingStacks).map(stack -> {
-				final DyeColor color = ColorUtil.getColorFromWool(Block.getBlockFromItem(stack.getItem()));
-				if (color != null && color != DyeColor.WHITE) {
-					return IDyeableItem.colorStack(recipe.getRecipeOutput().copy(), Arrays.asList(color));
-				}
-				return recipe.getRecipeOutput();
-			}).collect(Collectors.toList());
-			
-			outputs.stream().filter(stack -> !stack.hasTag()).forEach(stack -> System.out.println("NO STACK: " + stack.serializeNBT()));
-			
-			ingredients.setOutputLists(VanillaTypes.ITEM, Arrays.asList(outputs));
-		} else {
-			// ingredients.setOutputLists(VanillaTypes.ITEM, Arrays.asList(Arrays.asList(new ItemStack(Items.ACACIA_BOAT), new
-			// ItemStack(Items.ACACIA_DOOR))));
-			// ingredients.setOutput(VanillaTypes.ITEM, recipe.getRecipeOutput());
-		}
-		// ingredients.setOutput(VanillaTypes.ITEM, recipe.getRecipeOutput());
+		craftingGridHelper = new CraftingGridHelper(1);
 	}
 	
 	@Override
@@ -59,13 +39,53 @@ public class BackpackCraftingRecipeCategoryExtension implements ICraftingCategor
 		return new Size2i(recipe.getRecipeWidth(), recipe.getRecipeHeight());
 	}
 	
-	// @Override
-	// public void setRecipe(IRecipeLayout recipeLayout, IIngredients ingredients) {
-	// final List<List<ItemStack>> inputs = ingredients.getInputs(VanillaTypes.ITEM);
-	// final List<List<ItemStack>> outputs = ingredients.getOutputs(VanillaTypes.ITEM);
-	// recipeLayout.getItemStacks().set(0, outputs.get(0));
-	// // recipeLayout.getItemStacks().set(0, Arrays.asList(new ItemStack(Items.ACACIA_BOAT), new
-	// // ItemStack(Items.ACACIA_DOOR)));
-	// }
+	@Override
+	public void setIngredients(IIngredients ingredients) {
+		ingredients.setInputIngredients(recipe.getIngredients());
+		ingredients.setOutput(VanillaTypes.ITEM, recipe.getRecipeOutput());
+	}
 	
+	@Override
+	public void setRecipe(IRecipeLayout recipeLayout, IIngredients ingredients) {
+		final IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
+		
+		final List<List<ItemStack>> inputs = ingredients.getInputs(VanillaTypes.ITEM);
+		final List<List<ItemStack>> outputs = ingredients.getOutputs(VanillaTypes.ITEM);
+		
+		craftingGridHelper.setInputs(guiItemStacks, inputs, recipe.getWidth(), recipe.getHeight());
+		
+		final ItemStack outputStack = outputs.get(0).get(0);
+		
+		// Handle when focused on wool to display only that color
+		
+		final IFocus<?> focus = recipeLayout.getFocus();
+		if (focus != null && focus.getValue() instanceof ItemStack) {
+			final ItemStack focusStack = (ItemStack) focus.getValue();
+			final DyeColor color = ColorUtil.getColorFromWool(Block.getBlockFromItem(focusStack.getItem()));
+			if (color != null && outputStack.getItem() instanceof IDyeableItem) {
+				if (color == DyeColor.WHITE) {
+					guiItemStacks.set(0, outputStack);
+				} else {
+					guiItemStacks.set(0, IDyeableItem.colorStack(outputStack, Arrays.asList(color)));
+				}
+				return;
+			}
+		}
+		
+		// Handle the color rotation
+		
+		final Optional<Ingredient> oneWoolIngredient = recipe.getIngredients().stream().filter(ingredient -> ingredient.test(new ItemStack(Blocks.WHITE_WOOL))).findAny();
+		if (oneWoolIngredient.isPresent()) {
+			final List<ItemStack> outputStacks = Stream.of(oneWoolIngredient.get().getMatchingStacks()).map(stack -> {
+				final DyeColor color = ColorUtil.getColorFromWool(Block.getBlockFromItem(stack.getItem()));
+				if (color != null && outputStack.getItem() instanceof IDyeableItem && color != DyeColor.WHITE) {
+					return IDyeableItem.colorStack(outputStack, Arrays.asList(color));
+				}
+				return outputStack;
+			}).collect(Collectors.toList());
+			guiItemStacks.set(0, outputStacks);
+		} else {
+			guiItemStacks.set(0, outputStack);
+		}
+	}
 }
