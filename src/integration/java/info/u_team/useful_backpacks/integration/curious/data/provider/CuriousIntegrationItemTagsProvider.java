107b98291535e72130f5d8bd9ee596b4f46efcbc
HyCraftHD <hycrafthd@live.de>
2020-07-29 04:34:59 +0200

    Add tag for backpacks for the back slot

--- src/integration/java/info/u_team/useful_backpacks/integration/curious/data/provider/CuriousIntegrationItemTagsProvider.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curious/data/provider/CuriousIntegrationItemTagsProvider.java
@@ -1,6 +1,8 @@
 package info.u_team.useful_backpacks.integration.curious.data.provider;
 
 import info.u_team.u_team_core.data.*;
+import info.u_team.u_team_core.util.TagUtil;
+import info.u_team.useful_backpacks.init.UsefulBackpacksTags;
 
 public class CuriousIntegrationItemTagsProvider extends CommonItemTagsProvider {
 	
@@ -10,6 +12,7 @@ public class CuriousIntegrationItemTagsProvider extends CommonItemTagsProvider {
 	
 	@Override
 	protected void registerTags() {
+		getBuilder(TagUtil.createItemTag("curios", "back")).addTag(UsefulBackpacksTags.Items.BACKPACK);
 	}
 	
 }
