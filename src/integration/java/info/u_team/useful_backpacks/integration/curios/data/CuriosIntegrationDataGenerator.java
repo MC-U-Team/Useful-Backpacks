21f8b51c5aafe2080ced11557605614b687abd2b
HyCraftHD <hycrafthd@live.de>
2020-07-29 05:15:01 +0200

    Add curios item tag generator (not working currently)

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/data/CuriosIntegrationDataGenerator.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/data/CuriosIntegrationDataGenerator.java
@@ -3,7 +3,7 @@ package info.u_team.useful_backpacks.integration.curios.data;
 import info.u_team.u_team_core.data.GenerationData;
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
 import info.u_team.useful_backpacks.integration.curios.data.provider.CuriosIntegrationItemTagsProvider;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.*;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 @EventBusSubscriber(modid = UsefulBackpacksMod.MODID, bus = Bus.MOD)
 public class CuriosIntegrationDataGenerator {
 	
-	@SubscribeEvent
+	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void data(GatherDataEvent event) {
 		final GenerationData data = new GenerationData(UsefulBackpacksMod.MODID, event);
 		if (event.includeServer()) {
