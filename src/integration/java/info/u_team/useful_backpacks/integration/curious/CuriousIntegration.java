48a8ad0765b922dede9d9d93bf7e49476a24d8fe
HyCraftHD <hycrafthd@live.de>
2020-07-29 04:29:48 +0200

    Add basic curious integration to "register" our slot

---
+++ src/integration/java/info/u_team/useful_backpacks/integration/curious/CuriousIntegration.java
@@ -0,0 +1,19 @@
+package info.u_team.useful_backpacks.integration.curious;
+
+import info.u_team.useful_backpacks.UsefulBackpacksMod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import top.theillusivec4.curios.api.*;
+
+@EventBusSubscriber(modid = UsefulBackpacksMod.MODID, bus = Bus.MOD)
+public class CuriousIntegration {
+	
+	@SubscribeEvent
+	public static void enqueue(InterModEnqueueEvent event) {
+		InterModComms.sendTo("curios", "register_type", () -> SlotTypePreset.BACK.getMessageBuilder().build());
+	}
+	
+}
