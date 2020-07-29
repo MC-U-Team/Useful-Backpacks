bab8f46d653e19ff7ac3dd3fb64beb4d7772f469
HyCraftHD <hycrafthd@live.de>
2020-07-29 04:32:34 +0200

    Add curious item tags provider

---
+++ src/integration/java/info/u_team/useful_backpacks/integration/curious/data/provider/CuriousIntegrationItemTagsProvider.java
@@ -0,0 +1,15 @@
+package info.u_team.useful_backpacks.integration.curious.data.provider;
+
+import info.u_team.u_team_core.data.*;
+
+public class CuriousIntegrationItemTagsProvider extends CommonItemTagsProvider {
+	
+	public CuriousIntegrationItemTagsProvider(GenerationData data) {
+		super(data);
+	}
+	
+	@Override
+	protected void registerTags() {
+	}
+	
+}
