9c61b915d2c08e031c2b530504dd05874ffdede5
HyCraftHD <hycrafthd@live.de>
2020-08-01 20:01:34 +0200

    Add test for integration

---
+++ src/integration/java/info/u_team/useful_backpacks/move_to_uteamcore/Test.java
@@ -0,0 +1,8 @@
+package info.u_team.useful_backpacks.move_to_uteamcore;
+
+import info.u_team.useful_backpacks.UsefulBackpacksMod;
+
+@IIntegration(baseMod = UsefulBackpacksMod.MODID, integrationMod = "curios")
+public class Test {
+	
+}
