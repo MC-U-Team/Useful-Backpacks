222ab09e034c69d26a35d891c80b5fa888dab2e0
HyCraftHD <hycrafthd@live.de>
2020-08-01 20:39:42 +0200

    Init the integration now

--- src/integration/java/info/u_team/useful_backpacks/move_to_uteamcore/IntegrationManager.java
+++ src/integration/java/info/u_team/useful_backpacks/move_to_uteamcore/IntegrationManager.java
@@ -3,12 +3,15 @@ package info.u_team.useful_backpacks.move_to_uteamcore;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.*;
 import org.objectweb.asm.Type;
 
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.forgespi.language.ModFileScanData.AnnotationData;
 
 public class IntegrationManager {
+	
+	private static final Logger LOGGER = LogManager.getLogger();
 	
 	public static void callIntegrations(String modid) {
 		final Type type = Type.getType(IIntegration.class);
@@ -22,8 +25,12 @@ public class IntegrationManager {
 				.filter(data -> type.equals(data.getAnnotationType())) //
 				.collect(Collectors.toList());
 		
-		annotations.forEach(data -> {
-//			data.getClassType().
-		});
+		for (AnnotationData data : annotations) {
+			try {
+				Class.forName(data.getMemberName()).asSubclass(IModIntegration.class).newInstance().construct();
+			} catch (Exception ex) {
+				LOGGER.error("Failed to load and construct integration : {}", data.getMemberName(), ex);
+			}
+		}
 	}
 }
