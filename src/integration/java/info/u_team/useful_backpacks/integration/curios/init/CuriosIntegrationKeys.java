cc45cd505e91361fc45d7fd05159bfb8ffa00de8
HyCraftHD <hycrafthd@live.de>
2020-08-01 17:49:33 +0200

    Key works now. Changed from b to v cuz of narator

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationKeys.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationKeys.java
@@ -1,28 +1,37 @@
 package info.u_team.useful_backpacks.integration.curios.init;
 
+import java.util.Optional;
+
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.lwjgl.glfw.GLFW;
 
 import info.u_team.u_team_core.util.registry.ClientRegistry;
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
+import info.u_team.useful_backpacks.item.*;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.settings.IKeyConflictContext;
+import net.minecraftforge.event.TickEvent.*;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import top.theillusivec4.curios.api.CuriosApi;
 
 @EventBusSubscriber(modid = UsefulBackpacksMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class CuriosIntegrationKeys {
 	
-	public static final KeyBinding OPEN_BACKPACK = new KeyBinding("key.usefulbackpacks.curiosintegration.description", GLFW.GLFW_KEY_B, "key.usefulbackpacks.curiosintegration.category");
+	public static final KeyBinding OPEN_BACKPACK = new KeyBinding("key.usefulbackpacks.curiosintegration.description", GLFW.GLFW_KEY_V, "key.usefulbackpacks.curiosintegration.category");
 	
 	static {
 		OPEN_BACKPACK.setKeyConflictContext(new IKeyConflictContext() {
 			
 			@Override
 			public boolean isActive() {
-				return false;
+				return getBackpack(Minecraft.getInstance().player).isPresent();
 			}
 			
 			@Override
@@ -32,9 +41,27 @@ public class CuriosIntegrationKeys {
 		});
 	}
 	
+	public static Optional<ImmutableTriple<String, Integer, ItemStack>> getBackpack(LivingEntity livingEntity) {
+		return CuriosApi.getCuriosHelper().findEquippedCurio(stack -> stack.getItem() instanceof BackpackItem || stack.getItem() instanceof EnderChestBackpackItem, livingEntity);
+	}
+	
 	@SubscribeEvent
 	public static void register(FMLClientSetupEvent event) {
 		ClientRegistry.registerKeybinding(OPEN_BACKPACK);
+	}
+	
+	@EventBusSubscriber(modid = UsefulBackpacksMod.MODID, bus = Bus.FORGE, value = Dist.CLIENT)
+	public static class Handler {
+		
+		@SubscribeEvent
+		public static void onKeyPress(ClientTickEvent event) {
+			if (event.phase != Phase.END) {
+				return;
+			}
+			if (Minecraft.getInstance().isGameFocused() && OPEN_BACKPACK.isKeyDown()) {
+				System.out.println("PRESS | TICK");
+			}
+		}
 	}
 	
 }
