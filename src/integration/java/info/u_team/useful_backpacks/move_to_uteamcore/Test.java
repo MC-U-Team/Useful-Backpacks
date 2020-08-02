c1b1fc22b36dedf492a4366892c3d11e4471084c
HyCraftHD <hycrafthd@live.de>
2020-08-02 16:32:46 +0200

    Works now :)

--- src/integration/java/info/u_team/useful_backpacks/move_to_uteamcore/Test.java
+++ src/integration/java/info/u_team/useful_backpacks/move_to_uteamcore/Test.java
@@ -1,6 +1,23 @@
 package info.u_team.useful_backpacks.move_to_uteamcore;
 
 @IIntegration("curios")
-public class Test {
+public class Test implements IModIntegration {
+	
+	static {
+		System.out.println("---------------------------------- LOADED");
+	}
+	
+	{
+		System.out.println("XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+	}
+	
+	public Test() {
+		System.out.println("CTOR SDFJOESDZ LSDJKFHDK LHSJ");
+	}
+	
+	@Override
+	public void construct() {
+		System.out.println("CALL if curios is there");
+	}
 	
 }
