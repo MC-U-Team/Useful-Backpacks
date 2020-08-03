7a35f771dc70cae4b96250f472f57d7290ee4534
HyCraftHD <hycrafthd@live.de>
2020-08-03 16:13:04 +0200

    Add inter mod comms class

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/CuriosIntegration.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/CuriosIntegration.java
@@ -2,24 +2,24 @@ package info.u_team.useful_backpacks.integration.curios;
 
 import info.u_team.u_team_core.api.integration.*;
 import info.u_team.u_team_core.util.registry.BusRegister;
-import info.u_team.useful_backpacks.integration.curios.init.CuriosIntegrationKeys;
+import info.u_team.useful_backpacks.integration.curios.init.*;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.*;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import top.theillusivec4.curios.api.SlotTypePreset;
+import top.theillusivec4.curios.api.*;
 
 @Integration("curios")
 public class CuriosIntegration implements IModIntegration {
 	
 	@SubscribeEvent
 	public static void enqueue(InterModEnqueueEvent event) {
-		InterModComms.sendTo("curios", "register_type", () -> SlotTypePreset.BACK.getMessageBuilder().build());
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.BACK.getMessageBuilder().build());
 	}
 	
 	@Override
 	public void construct() {
 		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () -> BusRegister.registerMod(CuriosIntegrationKeys::register));
+		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () -> BusRegister.registerMod(CuriosIntegrationModComms::register));
 	}
-	
 }
---
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationModComms.java
@@ -0,0 +1,10 @@
+package info.u_team.useful_backpacks.integration.curios.init;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+
+public class CuriosIntegrationModComms {
+	
+	public static void register(IEventBus bus) {
+	}
+	
+}
