c6d68c7ca65069238f7f5604acb0ac9322a2fd90
HyCraftHD <hycrafthd@live.de>
2020-08-03 16:13:17 +0200

    Remove old comms

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/CuriosIntegration.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/CuriosIntegration.java
@@ -4,18 +4,10 @@ import info.u_team.u_team_core.api.integration.*;
 import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.useful_backpacks.integration.curios.init.*;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.*;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import top.theillusivec4.curios.api.*;
+import net.minecraftforge.fml.DistExecutor;
 
 @Integration("curios")
 public class CuriosIntegration implements IModIntegration {
-	
-	@SubscribeEvent
-	public static void enqueue(InterModEnqueueEvent event) {
-		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.BACK.getMessageBuilder().build());
-	}
 	
 	@Override
 	public void construct() {
