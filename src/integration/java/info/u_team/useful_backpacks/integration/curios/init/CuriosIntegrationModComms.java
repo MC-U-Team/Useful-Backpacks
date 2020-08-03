02032db4de8ad93a499fb77fa8810a46c45900df
HyCraftHD <hycrafthd@live.de>
2020-08-03 16:14:40 +0200

    Add the inter mod comms now back

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationModComms.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationModComms.java
@@ -1,10 +1,18 @@
 package info.u_team.useful_backpacks.integration.curios.init;
 
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import top.theillusivec4.curios.api.*;
 
 public class CuriosIntegrationModComms {
 	
+	private static void enqueue(InterModEnqueueEvent event) {
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.BACK.getMessageBuilder().build());
+	}
+	
 	public static void register(IEventBus bus) {
+		bus.addListener(CuriosIntegrationModComms::enqueue);
 	}
 	
 }
