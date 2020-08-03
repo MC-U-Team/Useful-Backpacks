63375a50d3691ab0ecd76c4f3f9e324679357ea4
HyCraftHD <hycrafthd@live.de>
2020-08-03 15:57:18 +0200

    Call the register ieventbus method now

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/CuriosIntegration.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/CuriosIntegration.java
@@ -1,8 +1,11 @@
 package info.u_team.useful_backpacks.integration.curios;
 
 import info.u_team.u_team_core.api.integration.*;
+import info.u_team.u_team_core.util.registry.BusRegister;
+import info.u_team.useful_backpacks.integration.curios.init.CuriosIntegrationKeys;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.*;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import top.theillusivec4.curios.api.SlotTypePreset;
 
@@ -13,9 +16,10 @@ public class CuriosIntegration implements IModIntegration {
 	public static void enqueue(InterModEnqueueEvent event) {
 		InterModComms.sendTo("curios", "register_type", () -> SlotTypePreset.BACK.getMessageBuilder().build());
 	}
-
+	
 	@Override
 	public void construct() {
+		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () -> BusRegister.registerMod(CuriosIntegrationKeys::register));
 	}
 	
 }
