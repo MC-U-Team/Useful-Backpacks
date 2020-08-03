2d37c6d0fe0cfe2b805772fa18d76f5ee49d7faa
HyCraftHD <hycrafthd@live.de>
2020-08-03 15:49:03 +0200

    Implement the interface

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/CuriosIntegration.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/CuriosIntegration.java
@@ -1,17 +1,21 @@
 package info.u_team.useful_backpacks.integration.curios;
 
-import info.u_team.u_team_core.api.integration.Integration;
+import info.u_team.u_team_core.api.integration.*;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import top.theillusivec4.curios.api.SlotTypePreset;
 
 @Integration("curios")
-public class CuriosIntegration {
+public class CuriosIntegration implements IModIntegration {
 	
 	@SubscribeEvent
 	public static void enqueue(InterModEnqueueEvent event) {
 		InterModComms.sendTo("curios", "register_type", () -> SlotTypePreset.BACK.getMessageBuilder().build());
+	}
+
+	@Override
+	public void construct() {
 	}
 	
 }
