b8ba9074ee8e6c644c07d41ba5f42f6fb7640615
HyCraftHD <hycrafthd@live.de>
2020-08-12 16:00:12 +0200

    Remove the config from the main mod class

--- src/main/java/info/u_team/useful_backpacks/UsefulBackpacksMod.java
+++ src/main/java/info/u_team/useful_backpacks/UsefulBackpacksMod.java
@@ -2,10 +2,7 @@ package info.u_team.useful_backpacks;
 
 import info.u_team.u_team_core.util.annotation.AnnotationManager;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
-import info.u_team.useful_backpacks.config.ServerConfig;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Mod(UsefulBackpacksMod.MODID)
 public class UsefulBackpacksMod {
@@ -14,7 +11,7 @@ public class UsefulBackpacksMod {
 	
 	public UsefulBackpacksMod() {
 		JarSignVerifier.checkSigned(MODID);
-		ModLoadingContext.get().registerConfig(Type.SERVER, ServerConfig.CONFIG);
+		
 		AnnotationManager.callAnnotations(MODID);
 	}
 }
