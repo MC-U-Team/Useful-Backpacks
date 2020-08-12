50d2ffa5a7284ca423c80979a30a849da3ac63b2
HyCraftHD <hycrafthd@live.de>
2020-08-12 15:59:18 +0200

    Add the basic register stuff to the construct common

--- src/main/java/info/u_team/useful_backpacks/init/UsefulBackpacksCommonConstruct.java
+++ src/main/java/info/u_team/useful_backpacks/init/UsefulBackpacksCommonConstruct.java
@@ -1,6 +1,7 @@
 package info.u_team.useful_backpacks.init;
 
 import info.u_team.u_team_core.api.construct.*;
+import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
 
 @Construct(modid = UsefulBackpacksMod.MODID)
@@ -8,6 +9,9 @@ public class UsefulBackpacksCommonConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		BusRegister.registerMod(UsefulBackpacksContainerTypes::register);
+		BusRegister.registerMod(UsefulBackpacksItems::register);
+		BusRegister.registerMod(UsefulBackpacksRecipeSerializers::register);
 	}
 	
 }
