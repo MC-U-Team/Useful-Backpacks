c0732980ded33a749211cfc7c9a93e48c72c40ee
HyCraftHD <hycrafthd@live.de>
2020-08-12 19:47:26 +0200

    Render the backpack on the back now

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/capability/CuriosBackpackCapability.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/capability/CuriosBackpackCapability.java
@@ -7,7 +7,9 @@ import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
+import net.minecraft.util.math.vector.*;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 
 public class CuriosBackpackCapability implements ICurio {
@@ -25,10 +27,20 @@ public class CuriosBackpackCapability implements ICurio {
 	
 	@Override
 	public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+		if (livingEntity.hasItemInSlot(EquipmentSlotType.CHEST)) {
+			return;
+		}
+		
 		matrixStack.push();
 		
 		RenderHelper.translateIfSneaking(matrixStack, livingEntity);
 		RenderHelper.rotateIfSneaking(matrixStack, livingEntity);
+		
+		matrixStack.rotate(Vector3f.XN.rotation((float) Math.PI));
+		
+		matrixStack.translate(0, -0.3, -0.14);
+		
+		matrixStack.scale(0.6F, 0.6F, 0.6F);
 		
 		Minecraft.getInstance().getItemRenderer().renderItem(stack, ItemCameraTransforms.TransformType.FIXED, 0xF000F0, OverlayTexture.NO_OVERLAY, matrixStack, renderTypeBuffer);
 		
