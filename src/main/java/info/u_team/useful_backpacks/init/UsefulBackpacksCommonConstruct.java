586bcd3b1fafec02de6ab4c021c067dafe1e6561
HyCraftHD <hycrafthd@live.de>
2020-08-12 16:00:03 +0200

    Add the config register in the common construct too

--- src/main/java/info/u_team/useful_backpacks/init/UsefulBackpacksCommonConstruct.java
+++ src/main/java/info/u_team/useful_backpacks/init/UsefulBackpacksCommonConstruct.java
@@ -3,12 +3,17 @@ package info.u_team.useful_backpacks.init;
 import info.u_team.u_team_core.api.construct.*;
 import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
+import info.u_team.useful_backpacks.config.ServerConfig;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Construct(modid = UsefulBackpacksMod.MODID)
 public class UsefulBackpacksCommonConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		ModLoadingContext.get().registerConfig(Type.SERVER, ServerConfig.CONFIG);
+		
 		BusRegister.registerMod(UsefulBackpacksContainerTypes::register);
 		BusRegister.registerMod(UsefulBackpacksItems::register);
 		BusRegister.registerMod(UsefulBackpacksRecipeSerializers::register);
