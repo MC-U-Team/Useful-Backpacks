1d23654a2a722dda8172a0b154c737c3f51655ba
HyCraftHD <hycrafthd@live.de>
2020-08-12 19:07:13 +0200

    Add curios backpack capability

---
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/capability/CuriosBackpackCapability.java
@@ -0,0 +1,11 @@
+package info.u_team.useful_backpacks.integration.curios.capability;
+
+import net.minecraft.item.ItemStack;
+import top.theillusivec4.curios.api.type.capability.ICurio;
+
+public class CuriosBackpackCapability implements ICurio {
+	
+	public CuriosBackpackCapability(ItemStack stack) {
+	}
+	
+}
