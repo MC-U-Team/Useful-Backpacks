beead17de25b6f529ccdf4c1cb6659b6e854d87b
HyCraftHD <hycrafthd@live.de>
2020-08-12 16:27:20 +0200

    Register the mod comms

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationCommonConstruct.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationCommonConstruct.java
@@ -1,6 +1,7 @@
 package info.u_team.useful_backpacks.integration.curios.init;
 
 import info.u_team.u_team_core.api.integration.*;
+import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
 
 @Integration(modid = UsefulBackpacksMod.MODID, integration = "curios")
@@ -8,7 +9,7 @@ public class CuriosIntegrationCommonConstruct implements IModIntegration {
 	
 	@Override
 	public void construct() {
-		
+		BusRegister.registerMod(CuriosIntegrationModComms::registerMod);
 	}
 	
 }
