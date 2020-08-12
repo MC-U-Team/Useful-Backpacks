bef551555b2256975092c4f73aab8c6134da380c
HyCraftHD <hycrafthd@live.de>
2020-08-12 18:20:24 +0200

    Register the open backpack network message

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationNetwork.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationNetwork.java
@@ -1,10 +1,13 @@
 package info.u_team.useful_backpacks.integration.curios.init;
 
+import java.util.Optional;
+
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
+import info.u_team.useful_backpacks.integration.curios.network.OpenBackpackMessage;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.*;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 public class CuriosIntegrationNetwork {
@@ -14,6 +17,7 @@ public class CuriosIntegrationNetwork {
 	public static final SimpleChannel NETWORK = NetworkRegistry.newSimpleChannel(new ResourceLocation(UsefulBackpacksMod.MODID, "curios"), () -> PROTOCOL, PROTOCOL::equals, PROTOCOL::equals);
 	
 	public static void setup(FMLCommonSetupEvent event) {
+		NETWORK.registerMessage(0, OpenBackpackMessage.class, OpenBackpackMessage::encode, OpenBackpackMessage::decode, OpenBackpackMessage.Handler::handle, Optional.of(NetworkDirection.PLAY_TO_SERVER));
 	}
 	
 	public static void registerMod(IEventBus bus) {
