611acdd9b67bf9c7284de1c671d6f64abdd2d2e5
HyCraftHD <hycrafthd@live.de>
2020-08-12 16:09:01 +0200

    Implement integration for common for curios

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationCommonConstruct.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationCommonConstruct.java
@@ -1,6 +1,14 @@
 package info.u_team.useful_backpacks.integration.curios.init;
 
+import info.u_team.u_team_core.api.integration.*;
+import info.u_team.useful_backpacks.UsefulBackpacksMod;
 
-public class CuriosIntegrationCommonConstruct {
+@Integration(modid = UsefulBackpacksMod.MODID, integration = "curios")
+public class CuriosIntegrationCommonConstruct implements IModIntegration {
+	
+	@Override
+	public void construct() {
+		
+	}
 	
 }
