c9c423ec32104cc0a9ebafd7e4f52bfe3b649299
HyCraftHD <hycrafthd@live.de>
2020-08-12 19:22:58 +0200

    Render the backpack in the world now (not translated the right way currently)

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/capability/CuriosBackpackCapability.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/capability/CuriosBackpackCapability.java
@@ -2,14 +2,20 @@ package info.u_team.useful_backpacks.integration.curios.capability;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 
 public class CuriosBackpackCapability implements ICurio {
 	
+	private final ItemStack stack;
+	
 	public CuriosBackpackCapability(ItemStack stack) {
+		this.stack = stack;
 	}
 	
 	@Override
@@ -19,6 +25,14 @@ public class CuriosBackpackCapability implements ICurio {
 	
 	@Override
 	public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+		matrixStack.push();
+		
+		RenderHelper.translateIfSneaking(matrixStack, livingEntity);
+		RenderHelper.rotateIfSneaking(matrixStack, livingEntity);
+		
+		Minecraft.getInstance().getItemRenderer().renderItem(stack, ItemCameraTransforms.TransformType.FIXED, 0xF000F0, OverlayTexture.NO_OVERLAY, matrixStack, renderTypeBuffer);
+		
+		matrixStack.pop();
 	}
 	
 }
