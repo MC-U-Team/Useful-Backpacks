b43ab57eb82ddb7f11fbb71d5d5224a2ce170af6
HyCraftHD <hycrafthd@live.de>
2020-08-12 16:09:59 +0200

    Implement integration for client for curios

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationClientConstruct.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/init/CuriosIntegrationClientConstruct.java
@@ -1,6 +1,14 @@
 package info.u_team.useful_backpacks.integration.curios.init;
 
+import info.u_team.u_team_core.api.integration.*;
+import info.u_team.useful_backpacks.UsefulBackpacksMod;
 
-public class CuriosIntegrationClientConstruct {
+@Integration(modid = UsefulBackpacksMod.MODID, integration = "curios", client = true)
+public class CuriosIntegrationClientConstruct implements IModIntegration {
+	
+	@Override
+	public void construct() {
+		
+	}
 	
 }
