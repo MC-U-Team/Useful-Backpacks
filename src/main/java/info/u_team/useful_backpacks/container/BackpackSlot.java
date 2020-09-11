a2284adf97152cdaef6c33eb82fdff862a0579c7
HyCraftHD <hycrafthd@live.de>
2020-09-11 16:11:48 +0200

    Add isItemValid method with the config option to address #45

--- src/main/java/info/u_team/useful_backpacks/container/BackpackSlot.java
+++ src/main/java/info/u_team/useful_backpacks/container/BackpackSlot.java
@@ -1,12 +1,23 @@
 package info.u_team.useful_backpacks.container;
 
+import info.u_team.useful_backpacks.config.CommonConfig;
+import info.u_team.useful_backpacks.item.BackpackItem;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.ItemStack;
 
 public class BackpackSlot extends Slot {
 	
 	public BackpackSlot(IInventory inventoryIn, int index, int xPosition, int yPosition) {
 		super(inventoryIn, index, xPosition, yPosition);
+	}
+	
+	@Override
+	public boolean isItemValid(ItemStack stack) {
+		if (stack.getItem() instanceof BackpackItem) {
+			return CommonConfig.getInstance().allowStackingBackpacks.get();
+		}
+		return true;
 	}
 	
 }
