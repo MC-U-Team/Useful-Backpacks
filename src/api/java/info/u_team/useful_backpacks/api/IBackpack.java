369ab3003d43bf74a50f4bfec4a7eb17b481726b
HyCraftHD <hycrafthd@live.de>
2020-12-12 21:39:53 +0100

    Add getInventory method

--- src/api/java/info/u_team/useful_backpacks/api/IBackpack.java
+++ src/api/java/info/u_team/useful_backpacks/api/IBackpack.java
@@ -1,10 +1,12 @@
 package info.u_team.useful_backpacks.api;
 
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 
 public interface IBackpack {
 	
 	void open(ServerPlayerEntity player, ItemStack stack, int selectedSlot);
 	
+	IInventory getInventory(ServerPlayerEntity player, ItemStack stack);
 }
