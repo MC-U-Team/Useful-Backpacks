0a1f6af06be58c8b583ad0428e003c25643de837
HyCraftHD <hycrafthd@live.de>
2020-12-13 15:57:53 +0100

    Add methods for the slot

--- src/main/java/info/u_team/useful_backpacks/container/slot/BackpackFilterSlot.java
+++ src/main/java/info/u_team/useful_backpacks/container/slot/BackpackFilterSlot.java
@@ -1,19 +1,29 @@
 package info.u_team.useful_backpacks.container.slot;
 
 import info.u_team.useful_backpacks.api.IBackpack;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
 
 public class BackpackFilterSlot extends Slot {
 	
-	public BackpackFilterSlot(IInventory inventoryIn, int index, int xPosition, int yPosition) {
+	private final IInventory filterSlotInventory;
+	
+	public BackpackFilterSlot(IInventory filterSlotInventory, IInventory inventoryIn, int index, int xPosition, int yPosition) {
 		super(inventoryIn, index, xPosition, yPosition);
+		this.filterSlotInventory = filterSlotInventory;
 	}
 	
 	@Override
 	public boolean isItemValid(ItemStack stack) {
 		return stack.getItem() instanceof IBackpack;
+	}
+	
+	@Override
+	public ItemStack onTake(PlayerEntity player, ItemStack stack) {
+		filterSlotInventory.clear();
+		return super.onTake(player, stack);
 	}
 	
 }
--- src/main/java/info/u_team/useful_backpacks/container/slot/FilterSlot.java
+++ src/main/java/info/u_team/useful_backpacks/container/slot/FilterSlot.java
@@ -4,16 +4,26 @@ import info.u_team.useful_backpacks.api.*;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.api.distmarker.*;
 
 public class FilterSlot extends Slot {
 	
-	public FilterSlot(IInventory inventory, int index, int xPosition, int yPosition) {
+	private final IInventory backpackSlotInventory;
+	
+	public FilterSlot(IInventory backpackSlotInventory, IInventory inventory, int index, int xPosition, int yPosition) {
 		super(inventory, index, xPosition, yPosition);
+		this.backpackSlotInventory = backpackSlotInventory;
 	}
 	
 	@Override
 	public boolean isItemValid(ItemStack stack) {
 		return stack.getItem() instanceof IFilter;
+	}
+	
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public boolean isEnabled() {
+		return !backpackSlotInventory.isEmpty();
 	}
 	
 }
