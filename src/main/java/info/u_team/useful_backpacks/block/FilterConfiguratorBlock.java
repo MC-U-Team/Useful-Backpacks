00bcab6ae90825fd790b70685f80bb825d6efb15
HyCraftHD <hycrafthd@live.de>
2020-12-13 01:48:12 +0100

    Adjust properties a bit

--- src/main/java/info/u_team/useful_backpacks/block/FilterConfiguratorBlock.java
+++ src/main/java/info/u_team/useful_backpacks/block/FilterConfiguratorBlock.java
@@ -2,13 +2,14 @@ package info.u_team.useful_backpacks.block;
 
 import info.u_team.u_team_core.block.UBlock;
 import info.u_team.useful_backpacks.init.UsefulBackpacksItemGroups;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.common.ToolType;
 
 public class FilterConfiguratorBlock extends UBlock {
 	
 	public FilterConfiguratorBlock() {
-		super(UsefulBackpacksItemGroups.GROUP, Properties.create(Material.WOOD).harvestTool(ToolType.AXE).hardnessAndResistance(2));
+		super(UsefulBackpacksItemGroups.GROUP, Properties.create(Material.WOOD).harvestTool(ToolType.AXE).hardnessAndResistance(2.5F).sound(SoundType.WOOD));
 	}
 	
 }
