3f43f05871b2dea9307a648fb86dda76c3aa37fb
HyCraftHD <hycrafthd@live.de>
2020-12-13 02:15:57 +0100

    Add basic screen class

---
+++ src/main/java/info/u_team/useful_backpacks/screen/FilterConfiguratorScreen.java
@@ -0,0 +1,17 @@
+package info.u_team.useful_backpacks.screen;
+
+import info.u_team.u_team_core.screen.UBasicContainerScreen;
+import info.u_team.useful_backpacks.UsefulBackpacksMod;
+import info.u_team.useful_backpacks.container.FilterConfiguratorContainer;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+
+public class FilterConfiguratorScreen extends UBasicContainerScreen<FilterConfiguratorContainer> {
+	
+	private static final ResourceLocation BACKGROUND = new ResourceLocation(UsefulBackpacksMod.MODID, "textures/gui/filter_configurator.png");
+	
+	public FilterConfiguratorScreen(FilterConfiguratorContainer container, PlayerInventory playerInventory, ITextComponent title) {
+		super(container, playerInventory, title, BACKGROUND);
+	}
+}
