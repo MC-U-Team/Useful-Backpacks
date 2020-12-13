147c3aa34115e509ded299bfadc90fe4fda0c961
HyCraftHD <hycrafthd@live.de>
2020-12-13 02:52:52 +0100

    Add inventory

--- src/main/java/info/u_team/useful_backpacks/container/FilterConfiguratorContainer.java
+++ src/main/java/info/u_team/useful_backpacks/container/FilterConfiguratorContainer.java
@@ -1,13 +1,18 @@
 package info.u_team.useful_backpacks.container;
 
 import info.u_team.u_team_core.container.UContainer;
+import info.u_team.useful_backpacks.container.slot.*;
 import info.u_team.useful_backpacks.init.*;
 import net.minecraft.entity.player.*;
+import net.minecraft.inventory.*;
 import net.minecraft.util.IWorldPosCallable;
 
 public class FilterConfiguratorContainer extends UContainer {
 	
 	private final IWorldPosCallable worldPos;
+	
+	private final IInventory backpackSlotInventory = new Inventory(1);
+	private final IInventory filterSlotInventory = new Inventory(9);
 	
 	// Client
 	public FilterConfiguratorContainer(int id, PlayerInventory playerInventory) {
@@ -19,6 +24,8 @@ public class FilterConfiguratorContainer extends UContainer {
 		super(UsefulBackpacksContainerTypes.FILTER_CONFIGURATOR.get(), id);
 		this.worldPos = worldPos;
 		
+		appendInventory(backpackSlotInventory, BackpackFilterSlot::new, 1, 1, 10, 10);
+		appendInventory(filterSlotInventory, FilterSlot::new, 3, 3, 30, 30);
 		appendPlayerInventory(playerInventory, 8, 111);
 	}
 	
