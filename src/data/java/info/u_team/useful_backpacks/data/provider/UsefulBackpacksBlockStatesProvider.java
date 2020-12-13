d3d7ccc6f3df2bcff512c1511237308108b09161
HyCraftHD <hycrafthd@live.de>
2020-12-13 01:21:20 +0100

    Add cubeButtomTop model generation for filter configurator

--- src/data/java/info/u_team/useful_backpacks/data/provider/UsefulBackpacksBlockStatesProvider.java
+++ src/data/java/info/u_team/useful_backpacks/data/provider/UsefulBackpacksBlockStatesProvider.java
@@ -1,6 +1,11 @@
 package info.u_team.useful_backpacks.data.provider;
 
+import static info.u_team.useful_backpacks.init.UsefulBackpacksBlocks.FILTER_CONFIGURATOR;
+
 import info.u_team.u_team_core.data.*;
+import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.generators.ModelFile;
 
 public class UsefulBackpacksBlockStatesProvider extends CommonBlockStatesProvider {
 	
@@ -10,6 +15,16 @@ public class UsefulBackpacksBlockStatesProvider extends CommonBlockStatesProvide
 	
 	@Override
 	protected void registerStatesAndModels() {
+		simpleBlock(FILTER_CONFIGURATOR.get(), cubeBottomTop(FILTER_CONFIGURATOR.get()));
+	}
+	
+	private ModelFile cubeBottomTop(Block block) {
+		final ResourceLocation location = blockTexture(block);
+		return models().cubeBottomTop(getPath(FILTER_CONFIGURATOR.get()), extend(location, "_side"), extend(location, "_bottom"), extend(location, "_top"));
+	}
+	
+	private ResourceLocation extend(ResourceLocation location, String suffix) {
+		return new ResourceLocation(location.getNamespace(), location.getPath() + suffix);
 	}
 	
 }
