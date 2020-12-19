0882f4e1deba6fdcc4c823e9990baca680e64f0a
HyCraftHD <hycrafthd@live.de>
2020-12-19 18:16:27 +0100

    Remove slot clear (is done in the main container)

--- src/main/java/info/u_team/useful_backpacks/container/slot/BackpackFilterSlot.java
+++ src/main/java/info/u_team/useful_backpacks/container/slot/BackpackFilterSlot.java
@@ -2,7 +2,6 @@ package info.u_team.useful_backpacks.container.slot;
 
 import info.u_team.useful_backpacks.UsefulBackpacksMod;
 import info.u_team.useful_backpacks.api.IBackpack;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.*;
 import net.minecraft.item.ItemStack;
@@ -12,11 +11,8 @@ public class BackpackFilterSlot extends Slot {
 	
 	public static final ResourceLocation BACKGROUND = new ResourceLocation(UsefulBackpacksMod.MODID, "item/empty_backpack_filter_slot");
 	
-	private final IInventory filterSlotInventory;
-	
-	public BackpackFilterSlot(IInventory filterSlotInventory, IInventory inventoryIn, int index, int xPosition, int yPosition) {
+	public BackpackFilterSlot(IInventory inventoryIn, int index, int xPosition, int yPosition) {
 		super(inventoryIn, index, xPosition, yPosition);
-		this.filterSlotInventory = filterSlotInventory;
 		setBackground(PlayerContainer.LOCATION_BLOCKS_TEXTURE, BACKGROUND);
 	}
 	
@@ -24,11 +20,4 @@ public class BackpackFilterSlot extends Slot {
 	public boolean isItemValid(ItemStack stack) {
 		return stack.getItem() instanceof IBackpack;
 	}
-	
-	@Override
-	public ItemStack onTake(PlayerEntity player, ItemStack stack) {
-		filterSlotInventory.clear();
-		return super.onTake(player, stack);
-	}
-	
 }
