8d5cc4ee782297bc7ea369588fb767e011cf6783
HyCraftHD <hycrafthd@live.de>
2020-12-20 16:41:05 +0100

    Add screen for item filter

---
+++ src/main/java/info/u_team/useful_backpacks/screen/ItemFilterScreen.java
@@ -0,0 +1,17 @@
+package info.u_team.useful_backpacks.screen;
+
+import info.u_team.u_team_core.screen.UBasicContainerScreen;
+import info.u_team.useful_backpacks.UsefulBackpacksMod;
+import info.u_team.useful_backpacks.container.ItemFilterContainer;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+
+public class ItemFilterScreen extends UBasicContainerScreen<ItemFilterContainer> {
+	
+	private static final ResourceLocation BACKGROUND = new ResourceLocation(UsefulBackpacksMod.MODID, "textures/gui/item_filter.png");
+	
+	public ItemFilterScreen(ItemFilterContainer container, PlayerInventory playerInventory, ITextComponent title) {
+		super(container, playerInventory, title, BACKGROUND, 130, 176);
+	}
+}
