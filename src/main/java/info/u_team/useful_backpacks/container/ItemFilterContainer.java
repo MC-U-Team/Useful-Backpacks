2318dfbe1ee87c82eac6e4e1d144fd9740d7d04f
HyCraftHD <hycrafthd@live.de>
2020-12-21 17:47:10 +0100

    Add in slot click when the filter slot is clicked, that the items are set directly in the inventory (and therefore in the slot)

--- src/main/java/info/u_team/useful_backpacks/container/ItemFilterContainer.java
+++ src/main/java/info/u_team/useful_backpacks/container/ItemFilterContainer.java
@@ -7,11 +7,12 @@ import net.minecraft.entity.player.*;
 import net.minecraft.inventory.*;
 import net.minecraft.inventory.container.*;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.PacketBuffer;
 
 public class ItemFilterContainer extends UContainer {
 	
-	private final ItemStack stack;
+	private final ItemStack filterStack;
 	private final int selectedSlot;
 	
 	private final IInventory filterItemSlotInventory = new Inventory(1) {
@@ -27,30 +28,32 @@ public class ItemFilterContainer extends UContainer {
 		this(id, playerInventory, ItemStack.EMPTY, buffer.readVarInt());
 	}
 	
-	public ItemFilterContainer(int id, PlayerInventory playerInventory, ItemStack stack, int selectedSlot) {
+	public ItemFilterContainer(int id, PlayerInventory playerInventory, ItemStack filterStack, int selectedSlot) {
 		super(UsefulBackpacksContainerTypes.ITEM_FILTER.get(), id);
-		this.stack = stack;
+		this.filterStack = filterStack;
 		this.selectedSlot = selectedSlot;
 		
+		final CompoundNBT compound = filterStack.getChildTag("stack");
+		if (compound != null) {
+			filterItemSlotInventory.setInventorySlotContents(0, ItemStack.read(compound));
+		}
+		
 		appendInventory(filterItemSlotInventory, ItemFilterSlot::new, 1, 1, 10, 10);
-		appendPlayerInventory(playerInventory, 8, 84);
+		appendPlayerInventory(playerInventory, 8, 48);
 	}
 	
 	@Override
 	public void detectAndSendChanges() {
-		if (!stack.isEmpty()) {
-			final ItemStack stackToFilter = filterItemSlotInventory.getStackInSlot(0);
-			if (stackToFilter.isEmpty()) {
-				stack.removeChildTag("stack");
-			} else {
-				stackToFilter.write(stack.getOrCreateChildTag("stack"));
-			}
-		}
+		
 		super.detectAndSendChanges();
 	}
 	
 	@Override
 	public ItemStack slotClick(int slotId, int dragType, ClickType clickType, PlayerEntity player) {
+		if (slotId == 0) {
+			return filterSlotClick(dragType, clickType, player);
+		}
+		
 		Slot tmpSlot;
 		if (slotId >= 0 && slotId < inventorySlots.size()) {
 			tmpSlot = inventorySlots.get(slotId);
@@ -73,4 +76,32 @@ public class ItemFilterContainer extends UContainer {
 		return super.slotClick(slotId, dragType, clickType, player);
 	}
 	
+	private ItemStack filterSlotClick(int dragType, ClickType clickType, PlayerEntity player) {
+		final ItemStack stack;
+		
+		if (clickType == ClickType.THROW) {
+			filterItemSlotInventory.setInventorySlotContents(0, ItemStack.EMPTY);
+			stack = ItemStack.EMPTY;
+		} else if (clickType == ClickType.PICKUP || clickType == ClickType.CLONE) {
+			stack = player.inventory.getItemStack().copy();
+			stack.setCount(1);
+			filterItemSlotInventory.setInventorySlotContents(0, stack);
+		} else if (clickType == ClickType.SWAP) {
+			stack = player.inventory.getStackInSlot(dragType).copy();
+			stack.setCount(1);
+			filterItemSlotInventory.setInventorySlotContents(0, stack);
+		} else {
+			stack = ItemStack.EMPTY;
+		}
+		
+		if (!filterStack.isEmpty() && !player.world.isRemote()) {
+			final ItemStack stackToFilter = filterItemSlotInventory.getStackInSlot(0);
+			if (stackToFilter.isEmpty()) {
+				stack.removeChildTag("stack");
+			} else {
+				stackToFilter.write(stack.getOrCreateChildTag("stack"));
+			}
+		}
+		return stack;
+	}
 }
