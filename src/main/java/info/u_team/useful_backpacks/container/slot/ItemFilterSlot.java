6e60b91f30baaa6eab1620901474cf631f27d1e3
HyCraftHD <hycrafthd@live.de>
2020-12-21 17:46:44 +0100

    Change item filter slot to deny all modifications

--- src/main/java/info/u_team/useful_backpacks/container/slot/ItemFilterSlot.java
+++ src/main/java/info/u_team/useful_backpacks/container/slot/ItemFilterSlot.java
@@ -1,7 +1,9 @@
 package info.u_team.useful_backpacks.container.slot;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.ItemStack;
 
 public class ItemFilterSlot extends Slot {
 	
@@ -9,4 +11,27 @@ public class ItemFilterSlot extends Slot {
 		super(inventory, index, xPosition, yPosition);
 	}
 	
+	@Override
+	public ItemStack onTake(PlayerEntity player, ItemStack stack) {
+		return stack;
+	}
+	
+	@Override
+	public ItemStack decrStackSize(int amound) {
+		return ItemStack.EMPTY;
+	}
+	
+	@Override
+	public boolean isItemValid(ItemStack stack) {
+		return false;
+	}
+	
+	@Override
+	public void putStack(ItemStack is) {
+	}
+	
+	@Override
+	public boolean canTakeStack(PlayerEntity player) {
+		return false;
+	}
 }
