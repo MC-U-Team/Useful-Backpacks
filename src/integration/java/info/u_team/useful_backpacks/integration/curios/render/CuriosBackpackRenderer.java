5c9576a4d8f46126ed8a1612a2712c2ae6ca55ee
HyCraftHD <hycrafthd@live.de>
2023-03-17 00:57:02 +0100

    Port integrations. Remove jei plugin for now

--- src/integration/java/info/u_team/useful_backpacks/integration/curios/render/CuriosBackpackRenderer.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/curios/render/CuriosBackpackRenderer.java
@@ -6,12 +6,12 @@ import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
@@ -35,7 +35,7 @@ public class CuriosBackpackRenderer implements ICurioRenderer {
 		
 		poseStack.scale(0.6F, 0.6F, 0.6F);
 		
-		Minecraft.getInstance().getItemRenderer().renderStatic(stack, TransformType.FIXED, 0xF000F0, OverlayTexture.NO_OVERLAY, poseStack, renderTypeBuffer, 0);
+		Minecraft.getInstance().getItemRenderer().renderStatic(stack, ItemDisplayContext.FIXED, 0xF000F0, OverlayTexture.NO_OVERLAY, poseStack, renderTypeBuffer, livingEntity.getLevel(), 0);
 	}
 	
 }
--- src/integration/java/info/u_team/useful_backpacks/integration/jei/extension/BackpackCraftingRecipeCategoryExtension.java
+++ src/integration/java/info/u_team/useful_backpacks/integration/jei/extension/BackpackCraftingRecipeCategoryExtension.java
@@ -1,106 +1,107 @@
-package info.u_team.useful_backpacks.integration.jei.extension;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import com.google.common.collect.Lists;
-
-import info.u_team.u_team_core.api.dye.DyeableItem;
-import info.u_team.u_team_core.util.ColorUtil;
-import info.u_team.u_team_core.util.RGB;
-import info.u_team.useful_backpacks.recipe.BackpackCraftingRecipe;
-import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
-import mezz.jei.api.gui.ingredient.ICraftingGridHelper;
-import mezz.jei.api.recipe.IFocusGroup;
-import mezz.jei.api.recipe.RecipeIngredientRole;
-import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategoryExtension;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.world.item.DyeColor;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-
-public class BackpackCraftingRecipeCategoryExtension implements ICraftingCategoryExtension {
-	
-	private final BackpackCraftingRecipe recipe;
-	
-	public BackpackCraftingRecipeCategoryExtension(BackpackCraftingRecipe recipe) {
-		this.recipe = recipe;
-	}
-	
-	@Override
-	public ResourceLocation getRegistryName() {
-		return recipe.getId();
-	}
-	
-	@Override
-	public int getWidth() {
-		return recipe.getRecipeWidth();
-	}
-	
-	@Override
-	public int getHeight() {
-		return recipe.getRecipeHeight();
-	}
-	
-	@Override
-	public void setRecipe(IRecipeLayoutBuilder builder, ICraftingGridHelper craftingGridHelper, IFocusGroup focuses) {
-		final List<List<ItemStack>> inputs = recipe.getIngredients().stream().map(ingredient -> Lists.newArrayList(ingredient.getItems())).collect(Collectors.toCollection(ArrayList::new));
-		final List<ItemStack> outputs = Lists.newArrayList(recipe.getResultItem());
-		
-		final AtomicBoolean changed = new AtomicBoolean(false);
-		
-		focuses.getItemStackFocuses(RecipeIngredientRole.OUTPUT) //
-				.map(focus -> focus.getTypedValue().getIngredient()) //
-				.findFirst().ifPresent(outputStack -> {
-					if (outputStack.getItem() instanceof DyeableItem dyeable) {
-						final int dyeableColor = dyeable.getColor(outputStack);
-						final DyeColor color = ColorUtil.findClosestDyeColor(new RGB(dyeableColor));
-						final Block wool = ColorUtil.getWoolFromColor(color);
-						for (int index = 0; index < inputs.size(); index++) {
-							final List<ItemStack> list = inputs.get(index);
-							if (list.stream().allMatch(stack -> stack.is(ItemTags.WOOL))) {
-								if (dyeableColor != dyeable.getDefaultColor()) {
-									inputs.set(index, List.of(new ItemStack(wool)));
-								} else {
-									inputs.set(index, List.of(new ItemStack(Blocks.WHITE_WOOL)));
-								}
-							}
-						}
-						outputs.set(0, DyeableItem.colorStack(outputs.get(0), List.of(color)));
-						changed.set(true);
-					}
-				});
-		
-		focuses.getItemStackFocuses(RecipeIngredientRole.INPUT) //
-				.map(focus -> focus.getTypedValue().getIngredient()) //
-				.findFirst().ifPresent(inputStack -> {
-					if (inputStack.is(ItemTags.WOOL)) {
-						final DyeColor color = ColorUtil.getColorFromWool(inputStack.getItem());
-						if (color != null && color != DyeColor.WHITE) {
-							outputs.set(0, DyeableItem.colorStack(outputs.get(0), List.of(color)));
-						}
-						changed.set(true);
-					}
-				});
-		
-		if (!changed.get()) {
-			Stream.of(DyeColor.values()).map(color -> {
-				if (color == DyeColor.WHITE) {
-					return outputs.get(0);
-				}
-				return DyeableItem.colorStack(outputs.get(0), List.of(color));
-			}).forEach(outputs::add);
-			outputs.remove(0);
-		}
-		
-		craftingGridHelper.createAndSetInputs(builder, VanillaTypes.ITEM_STACK, inputs, getWidth(), getHeight());
-		craftingGridHelper.createAndSetOutputs(builder, VanillaTypes.ITEM_STACK, outputs);
-	}
-	
-}
+// package info.u_team.useful_backpacks.integration.jei.extension;
+//
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.concurrent.atomic.AtomicBoolean;
+// import java.util.stream.Collectors;
+// import java.util.stream.Stream;
+//
+// import com.google.common.collect.Lists;
+//
+// import info.u_team.u_team_core.api.dye.DyeableItem;
+// import info.u_team.u_team_core.util.ColorUtil;
+// import info.u_team.u_team_core.util.RGB;
+// import info.u_team.useful_backpacks.recipe.BackpackCraftingRecipe;
+// import mezz.jei.api.constants.VanillaTypes;
+// import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+// import mezz.jei.api.gui.ingredient.ICraftingGridHelper;
+// import mezz.jei.api.recipe.IFocusGroup;
+// import mezz.jei.api.recipe.RecipeIngredientRole;
+// import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategoryExtension;
+// import net.minecraft.resources.ResourceLocation;
+// import net.minecraft.tags.ItemTags;
+// import net.minecraft.world.item.DyeColor;
+// import net.minecraft.world.item.ItemStack;
+// import net.minecraft.world.level.block.Block;
+// import net.minecraft.world.level.block.Blocks;
+//
+// public class BackpackCraftingRecipeCategoryExtension implements ICraftingCategoryExtension {
+//
+// private final BackpackCraftingRecipe recipe;
+//
+// public BackpackCraftingRecipeCategoryExtension(BackpackCraftingRecipe recipe) {
+// this.recipe = recipe;
+// }
+//
+// @Override
+// public ResourceLocation getRegistryName() {
+// return recipe.getId();
+// }
+//
+// @Override
+// public int getWidth() {
+// return recipe.getRecipeWidth();
+// }
+//
+// @Override
+// public int getHeight() {
+// return recipe.getRecipeHeight();
+// }
+//
+// @Override
+// public void setRecipe(IRecipeLayoutBuilder builder, ICraftingGridHelper craftingGridHelper, IFocusGroup focuses) {
+// final List<List<ItemStack>> inputs = recipe.getIngredients().stream().map(ingredient ->
+// Lists.newArrayList(ingredient.getItems())).collect(Collectors.toCollection(ArrayList::new));
+// final List<ItemStack> outputs = Lists.newArrayList(recipe.getResultItem());
+//
+// final AtomicBoolean changed = new AtomicBoolean(false);
+//
+// focuses.getItemStackFocuses(RecipeIngredientRole.OUTPUT) //
+// .map(focus -> focus.getTypedValue().getIngredient()) //
+// .findFirst().ifPresent(outputStack -> {
+// if (outputStack.getItem() instanceof DyeableItem dyeable) {
+// final int dyeableColor = dyeable.getColor(outputStack);
+// final DyeColor color = ColorUtil.findClosestDyeColor(new RGB(dyeableColor));
+// final Block wool = ColorUtil.getWoolFromColor(color);
+// for (int index = 0; index < inputs.size(); index++) {
+// final List<ItemStack> list = inputs.get(index);
+// if (list.stream().allMatch(stack -> stack.is(ItemTags.WOOL))) {
+// if (dyeableColor != dyeable.getDefaultColor()) {
+// inputs.set(index, List.of(new ItemStack(wool)));
+// } else {
+// inputs.set(index, List.of(new ItemStack(Blocks.WHITE_WOOL)));
+// }
+// }
+// }
+// outputs.set(0, DyeableItem.colorStack(outputs.get(0), List.of(color)));
+// changed.set(true);
+// }
+// });
+//
+// focuses.getItemStackFocuses(RecipeIngredientRole.INPUT) //
+// .map(focus -> focus.getTypedValue().getIngredient()) //
+// .findFirst().ifPresent(inputStack -> {
+// if (inputStack.is(ItemTags.WOOL)) {
+// final DyeColor color = ColorUtil.getColorFromWool(inputStack.getItem());
+// if (color != null && color != DyeColor.WHITE) {
+// outputs.set(0, DyeableItem.colorStack(outputs.get(0), List.of(color)));
+// }
+// changed.set(true);
+// }
+// });
+//
+// if (!changed.get()) {
+// Stream.of(DyeColor.values()).map(color -> {
+// if (color == DyeColor.WHITE) {
+// return outputs.get(0);
+// }
+// return DyeableItem.colorStack(outputs.get(0), List.of(color));
+// }).forEach(outputs::add);
+// outputs.remove(0);
+// }
+//
+// craftingGridHelper.createAndSetInputs(builder, VanillaTypes.ITEM_STACK, inputs, getWidth(), getHeight());
+// craftingGridHelper.createAndSetOutputs(builder, VanillaTypes.ITEM_STACK, outputs);
+// }
+//
+// }
